50685e62310af51e1dddc69cdbf47ed9054c42d4
Carl Tanner <rocketeer.captproton@gmail.com>
2017-04-03 15:31:58 -0700

    WOOT: app builds and fastlane beta works

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org' do
     # Gems here
   gem 'faraday', '~> 0.12.0'
-  gem 'fastlane', '~> 2.24.0'
+  gem 'fastlane', '~> 2.25'
   gem "dotenv"
   gem "pry"
   gem 'rb-readline', '~> 0.5.4'
--- evo_agent/ViewController.swift
+++ evo_agent/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import WebKit
+import Crashlytics
+
 
 class ViewController: UIViewController {
+
+
+    override func loadView() {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
+
+        // let myURL = URL(string: "https://realestateflywheel-production.herokuapp.com")
+        // let myRequest = URLRequest(url: myURL!)
+        // webView.load(myRequest)
+		
+    }
+
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+        Crashlytics.sharedInstance().crash()
     }
 
     override func didReceiveMemoryWarning() {
